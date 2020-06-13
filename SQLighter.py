da6dead775bec7a17a08448c8ae056475afb6fab
LDmitriy7 <levchenko.d.a1998@gmail.com>
2020-06-13 18:49:18 +0300

    Add files via upload

---
+++ SQLighter.py
@@ -0,0 +1,149 @@
+import sqlite3
+from time import time as TIME
+
+
+class SQLighter:
+
+    def __init__(self, database):
+        """Подключение к БД и сохраненние курсора соединения"""
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+
+    def is_user_in_base(self, user_id):
+        """Проверка, есть ли юзер в БД"""
+        with self.connection:
+            result = self.cursor.execute("SELECT user_id FROM config WHERE user_id = ?", (user_id,)).fetchall()
+            return bool(len(result))
+
+    def add_user(self, user_id):
+        """Добавление юзера в БД"""
+        with self.connection:
+            return self.cursor.execute("INSERT INTO config VALUES(?,25,5,50,10)", (user_id,))
+
+    def drop_user(self, user_id):
+        """Удаление юзера из БД"""
+        with self.connection:
+            return self.cursor.execute("DELETE FROM config WHERE user_id = ?", (user_id,))
+
+    def set_config(self, user_id, w_time, r_time, w2_time, r2_time):
+        """Установка времени стандартных таймеров"""
+        if not self.is_user_in_base(user_id):
+            self.add_user(user_id)
+        with self.connection:
+            return self.cursor.execute(
+                "UPDATE config SET w_time = ?, r_time = ?, w2_time = ?, r2_time = ? WHERE user_id = ?",
+                (w_time, r_time, w2_time, r2_time, user_id))
+
+    def get_config(self, user_id):
+        """Получение установленного времени стандартных таймеров"""
+        if not self.is_user_in_base(user_id):
+            self.add_user(user_id)
+        with self.connection:
+            return self.cursor.execute(
+                "SELECT w_time, r_time, w2_time, r2_time FROM config WHERE user_id = ?", (user_id,)).fetchone()
+
+    def set_timer(self, user_id, min_dur, mode):
+        """Установка таймера"""
+        with self.connection:
+            self.cursor.execute("DELETE FROM timers_on WHERE user_id = ?", (user_id,))
+            return self.cursor.execute("INSERT INTO timers_on VALUES(?,?,?,?)",
+                                       (user_id, TIME() + min_dur * 60, min_dur, mode))
+
+    def give_points_and_del_timer(self, user_id, points, mode):
+        """Начисление таймреа в статистику и удаление"""
+        with self.connection:
+            if not self.cursor.execute("SELECT * FROM stats WHERE user_id = ?", (user_id,)).fetchone():
+                self.cursor.execute("INSERT INTO stats VALUES(?,0,0,0,0)", (user_id,))
+            if mode in ['w', 'W']:
+                self.cursor.execute("UPDATE stats SET w_timers = w_timers + 1, w_mins = w_mins + ? WHERE user_id = ?",
+                                    (points, user_id))
+            else:
+                self.cursor.execute("UPDATE stats SET r_timers = r_timers + 1, r_mins = r_mins + ? WHERE user_id = ?",
+                                    (points, user_id))
+            return self.cursor.execute("DELETE FROM timers_on WHERE user_id = ?", (user_id,))
+
+    def del_timer(self, user_id):
+        """Удаление таймера"""
+        with self.connection:
+            return self.cursor.execute("DELETE FROM timers_on WHERE user_id = ?", (user_id,))
+
+    def get_timer(self, user_id):
+        """Получение установленного времени таймера"""
+        with self.connection:
+            result = self.cursor.execute("SELECT f_time FROM timers_on WHERE user_id = ?", (user_id,)).fetchone()
+            if result:
+                result = result[0]
+            return result
+
+    def get_mode_from_timers_on(self, user_id):
+        """Получение режима заведенного таймера"""
+        with self.connection:
+            result = self.cursor.execute("SELECT mode FROM timers_on WHERE user_id = ?", (user_id,)).fetchone()
+            if result:
+                result = result[0]
+            return result
+
+    def check_timers(self):
+        """Проверка на законченные таймеры"""
+        with self.connection:
+            result = self.cursor.execute(
+                "SELECT * FROM timers_on WHERE f_time = (SELECT MIN(f_time) FROM timers_on)").fetchone()
+            if result:
+                user_id, f_time, points, mode = result
+                if TIME() >= f_time:
+                    return user_id, points, mode
+
+    def check_date(self):
+        """Проверка на необходимость обнуления статистики"""
+        with self.connection:
+            result = self.cursor.execute(
+                "SELECT st_time FROM timers_on WHERE f_time = (SELECT MIN(f_time) FROM timers_on)").fetchone()
+            if result:
+                user_id, f_time, points, mode = result
+                if TIME() >= f_time:
+                    return user_id, points, mode
+
+    def pause_timer(self, user_id):
+        """Приостановка таймера"""
+        with self.connection:
+            points, mode = self.cursor.execute("SELECT points, mode FROM timers_on WHERE user_id = ?",
+                                               (user_id,)).fetchone()
+            self.cursor.execute("DELETE FROM timers_off WHERE user_id = ?", (user_id,))
+            self.cursor.execute("INSERT INTO timers_off VALUES(?,?,?,?)",
+                                (user_id, self.get_timer(user_id) - TIME(), points, mode))
+            self.cursor.execute("DELETE FROM timers_on WHERE user_id = ?", (user_id,))
+            return mode
+
+    def resume_timer(self, user_id):
+        """Возобновление таймера"""
+        with self.connection:
+            duration = self.cursor.execute("SELECT duration FROM timers_off WHERE user_id = ?", (user_id,)).fetchone()
+            if duration:
+                duration = duration[0]
+                points, mode = self.cursor.execute("SELECT points, mode FROM timers_off WHERE user_id = ?",
+                                                   (user_id,)).fetchone()
+                self.cursor.execute("DELETE FROM timers_on WHERE user_id = ?", (user_id,))
+                self.cursor.execute("INSERT INTO timers_on VALUES(?,?,?,?)", (user_id, TIME() + duration, points, mode))
+                self.cursor.execute("DELETE FROM timers_off WHERE user_id = ?", (user_id,))
+                return duration
+
+    def get_stats(self, user_id):
+        """Получение статистики"""
+        with self.connection:
+            return self.cursor.execute("SELECT w_timers, r_timers, w_mins, r_mins FROM stats WHERE user_id = ?",
+                                       (user_id,)).fetchone()
+
+    def update_stats(self):
+        """Обнуление статистики"""
+        with self.connection:
+            return self.cursor.execute("UPDATE stats SET w_timers = 0, r_timers = 0, w_mins = 0, r_mins = 0")
+
+    def update_one_stat(self, user_id):
+        """Обнуление статистики по айди"""
+        with self.connection:
+            return self.cursor.execute(
+                "UPDATE stats SET w_timers = 0, r_timers = 0, w_mins = 0, r_mins = 0 WHERE user_id = ?", (user_id,))
+
+    def close(self):
+        """Закрытие соединение с БД"""
+        self.connection.close()
---
+++ bot.py
@@ -0,0 +1,228 @@
+import os
+import asyncio
+import config
+import re
+from time import time as TIME, sleep
+from aiogram import Bot, Dispatcher, executor, types
+from SQLighter import SQLighter
+from math import ceil
+
+# инициализируем бота
+token = os.environ.get('BOT_TOKEN')
+bot = Bot(token=token)
+dp = Dispatcher(bot)
+
+# инициализируем соединение с БД
+db = SQLighter('users.db')
+
+keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.row('W', 'R', 'W2', 'R2')
+keyboard.row('T', 'P', 'P2', 'S', 'S2', 'D')
+keyboard.row('/set_timers', '/get_timers')
+
+
+# стартовая инструкция
+@dp.message_handler(commands=['start', 'help'])
+async def set_time(message: types.Message):
+    await message.answer(config.start_text, disable_web_page_preview=True, reply_markup=keyboard)
+
+
+# открыть клавиатуру
+@dp.message_handler(commands=['keyboard'])
+async def set_time(message: types.Message):
+    await message.answer("Открываю клавиатуру", reply_markup=keyboard)
+
+
+# Установка стандартных таймеров
+@dp.message_handler(commands=['set_timers'])
+async def set_time(message: types.Message):
+    await message.answer("Введите через пробел 4 числа:\nжелаемое время для W, R, W2 и R2 соответственно (в минутах)",
+                         reply_markup=types.ForceReply())
+
+
+# Продолжение
+@dp.message_handler(lambda msg: msg.reply_to_message and msg.reply_to_message.text in [
+    "Введите через пробел 4 числа:\nжелаемое время для "
+    "W, R, W2 и R2 соответственно (в минутах)",
+    "Ошибка, введите 4 целых числа в пределах от 1 до 999"])
+async def set_time2(message: types.Message):
+    text = message.text.split()
+    if len(text) == 4:
+        if all(map(lambda x: x.isdigit() and int(x) in range(1, 1000), text)):
+            db.set_config(message.from_user.id, text[0], text[1], text[2], text[3])
+            await message.answer("Новые параметры успешно установлены", reply_markup=keyboard)
+        else:
+            await message.answer("Ошибка, введите 4 целых числа в пределах от 1 до 999",
+                                 reply_markup=types.ForceReply())
+    else:
+        await message.answer("Ошибка, введите 4 целых числа в пределах от 1 до 999", reply_markup=types.ForceReply())
+
+
+# Просмотр стандартных таймеров
+@dp.message_handler(commands=['get_timers'])
+async def get_time(message: types.Message):
+    result = db.get_config(message.from_user.id)
+    await message.answer(f"Сейчас установлено (минут):\nW={result[0]}, R={result[1]}, W2={result[2]}, R2={result[3]}")
+
+
+# получение статистики
+@dp.message_handler(lambda msg: msg.text.lower().replace('.', '') == 's')
+async def get_stats(msg: types.Message):
+    if msg.text[-1] == '.':
+        sleep(10)
+    result = db.get_stats(msg.from_user.id)
+    if result:
+        await msg.answer(
+            f"Сегодня вы:\n-------------------------------------------------\n  "
+            f"* Работали {result[2]} мин.\n    (закончено таймеров: {result[0]})\n\n"
+            f"  * Отдыхали {result[3]} мин.\n    (закончено таймеров: {result[1]})")
+    else:
+        await msg.answer("Сегодня еще нет законченных таймеров")
+
+
+# очистка статистики
+@dp.message_handler(lambda msg: msg.text.lower().replace('.', '') == 's2')
+async def get_stats(msg: types.Message):
+    if msg.text[-1] == '.':
+        sleep(10)
+    db.update_one_stat(msg.from_user.id)
+    await msg.answer("Статистика сброшена")
+
+
+# Заведение одного из стандартных таймеров
+@dp.message_handler(lambda msg: msg.text.lower().replace('.', '') in ('w', 'r', 'w2', 'r2'))
+async def start_timer(msg: types.Message):
+    if msg.text[-1] == '.':
+        sleep(10)
+    text = msg.text.lower().replace('.', '')
+    result = db.get_config(msg.from_user.id)
+    result = {'w': result[0], 'r': result[1], 'w2': result[2], 'r2': result[3], }
+    min_dur = result[text]
+    if text in ['w', 'w2']:
+        db.set_timer(msg.from_user.id, min_dur, 'w')
+        await msg.answer(f"Завожу {min_dur} мин. работы")
+    else:
+        db.set_timer(msg.from_user.id, min_dur, 'r')
+        await msg.answer(f"Завожу {min_dur} мин. отдыха")
+
+
+# Заведение произвольного таймера
+@dp.message_handler(lambda msg: re.fullmatch(r'\d+[w,W,r,R]\.?', msg.text))
+async def start_arb_timer(msg: types.Message):
+    integer = int(msg.text.replace('.', '')[:-1])
+    mode = msg.text.replace('.', '')[-1]
+    if msg.text[-1] == '.':
+        sleep(10)
+    if integer in range(1, 1000):
+        if mode in ['w', 'W']:
+            db.set_timer(msg.from_user.id, integer, 'w')
+            await msg.answer(f"Завожу {integer} мин. работы")
+        else:
+            db.set_timer(msg.from_user.id, integer, 'r')
+            await msg.answer(f"Завожу {integer} мин. отдыха")
+    else:
+        await msg.answer("Время должно быть в пределах от 1 до 999 минут")
+
+
+# Удаление текущего таймера
+@dp.message_handler(lambda msg: msg.text.lower().replace('.', '') == 'd')
+async def del_timer(msg: types.Message):
+    if msg.text[-1] == '.':
+        sleep(10)
+    db.del_timer(msg.from_user.id)
+    await msg.answer("Таймер удален")
+
+
+# Просмотр оставшегося времени текущего таймера
+@dp.message_handler(lambda msg: msg.text.lower().replace('.', '') == 't')
+async def get_timer(msg: types.Message):
+    time = db.get_timer(msg.from_user.id)
+    if msg.text[-1] == '.':
+        sleep(10)
+    if time:
+        result = ceil((time - TIME()) / 60)
+        if db.get_mode_from_timers_on(msg.from_user.id) in ['w', 'W']:
+            await msg.answer(f"Осталось меньше {result} мин. работы")
+        else:
+            await msg.answer(f"Осталось меньше {result} мин. отдыха")
+    else:
+        await msg.answer("Нет заведенных таймеров")
+
+
+# Приостановка таймера
+@dp.message_handler(lambda msg: msg.text.lower().replace('.', '') == 'p')
+async def pause(msg: types.Message):
+    time = db.get_timer(msg.from_user.id)
+    if msg.text[-1] == '.':
+        sleep(10)
+    if time:
+        result = ceil((time - TIME()) / 60)
+        if db.pause_timer(msg.from_user.id) in ['w', 'W']:
+            await msg.answer(f"Таймер приостановлен, оставалось меньше {result} мин. работы")
+        else:
+            await msg.answer(f"Таймер приостановлен, оставалось меньше {result} мин. отдыха")
+    else:
+        await msg.answer("Нет заведенных таймеров")
+
+
+# Возобновение таймера
+@dp.message_handler(lambda msg: msg.text.lower().replace('.', '') == 'p2')
+async def pause(msg: types.Message):
+    duration = db.resume_timer(msg.from_user.id)
+    if msg.text[-1] == '.':
+        sleep(10)
+    if duration:
+        min_dur = round(duration / 60)
+        if db.get_mode_from_timers_on(msg.from_user.id) in ['w', 'W']:
+            await msg.answer(f"Таймер возобновлен, завожу около {min_dur} мин. работы")
+        else:
+            await msg.answer(f"Таймер возобновлен, завожу около {min_dur} мин. отдыха")
+    else:
+        await msg.answer("Нет приостановленных таймеров")
+
+
+@dp.message_handler(lambda msg: True)
+async def answer(msg: types.Message):
+    time = db.get_timer(msg.from_user.id)
+    if msg.text[-1] == '.':
+        sleep(10)
+    if time:
+        result = ceil((time - TIME()) / 60)
+        if db.get_mode_from_timers_on(msg.from_user.id) in ['w', 'W']:
+            await msg.answer(f"Осталось меньше {result} мин. работы")
+        else:
+            await msg.answer(f"Осталось меньше {result} мин. отдыха")
+    else:
+        await msg.answer("Нет заведенных таймеров")
+
+
+# проверка вышедших таймеров
+async def check(wait_for):
+    while True:
+        await asyncio.sleep(wait_for)
+        result = db.check_timers()
+        if result:
+            user_id, points, mode = result
+            db.give_points_and_del_timer(user_id, points, mode)
+            if mode in ['w', 'W']:
+                await bot.send_message(user_id, f"Время таймера вышло, начислено {points} мин. работы")
+            else:
+                await bot.send_message(user_id, f"Время таймера вышло, начислено {points} мин. отдыха")
+
+
+# проверка необходимости обновления статистики
+async def check_date(wait_for):
+    while True:
+        await asyncio.sleep(wait_for)
+        with open('update_stats.txt', 'r', encoding='utf-8') as f:
+            if TIME() - float(f.readline()) >= 3600 * 24:
+                with open('update_stats.txt', 'w', encoding='utf-8') as f2:
+                    print(TIME(), file=f2)
+                db.update_stats()
+
+
+# запускаем лонг-поллинг
+if __name__ == '__main__':
+    dp.loop.create_task(check(1))
+    dp.loop.create_task(check_date(1))
+    executor.start_polling(dp, skip_updates=True)
